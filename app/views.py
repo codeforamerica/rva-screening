e749e67f0a76af5793284f963c1e8be40f9873cc
Emma Smithayer <esmithayer@gmail.com>
2015-03-25 11:17:55 -0700

    Add update and delete

--- app/views.py
+++ app/views.py
@@ -21,6 +21,32 @@ def add():
 
   return render_template('add.html')
 
+@app.route('/update/<id>', methods=['POST', 'GET'])
+def update(id):
+  patient = Patient.query.get(id)
+  print "request method id"
+  print request.method
+  if request.method == 'POST':
+    patient.firstname = request.form['firstname'],
+    patient.middlename = request.form['middlename'],
+    patient.lastname = request.form['lastname'],
+    patient.dob = datetime.datetime.strptime(request.form['dob'], '%m/%d/%Y').date(),
+    patient.phonenumber1 = request.form['phonenumber1'],
+    patient.phonenumber2 = request.form['phonenumber2']
+    db.session.commit()
+    flash('Patient updated!')
+    return redirect(url_for('index'))
+
+  return render_template('update.html', patient=patient)
+
+@app.route('/delete/<id>', methods=['POST', 'GET'])
+def delete(id):
+  patient = Patient.query.get(id)
+  db.session.delete(patient)
+  db.session.commit()
+  flash('Patient deleted!')
+  return redirect(url_for('index'))
+
 @app.route('/' )
 def index():
   patients = Patient.query.all()    
