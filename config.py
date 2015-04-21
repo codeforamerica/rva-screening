8b9258a371dccb2020e36a28510a418a6648a5f5
svmatthews <matthews.sam@gmail.com>
2015-04-21 15:14:51 -0700

    jekyll build for view prototyping

--- config.py
+++
@@ -1,3 +0,0 @@
-import os
-
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql://root:password@localhost/rva_screening')
--- run.py
+++
@@ -1,3 +0,0 @@
-from app import app
-app.debug = True
-app.run()
