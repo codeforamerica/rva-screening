7c7acbc6f1c775253a2b8f2e8b25f6a5cae815a4
Emma Smithayer <esmithayer@gmail.com>
2015-04-28 16:49:04 -0700

    Updated routes and templates to align with diagrams in issue #19

--- app/models.py
+++ app/models.py
@@ -3,29 +3,19 @@ from app import db
 class Patient(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   firstname = db.Column(db.String(64))
-  middlename = db.Column(db.String(64))
+  middlename = db.Column(db.String(64), nullable=True)
   lastname = db.Column(db.String(64))
   dob = db.Column(db.Date())
-  phonenumber1 = db.Column(db.String(32))
-  phonenumber2 = db.Column(db.String(32))
+  phonenumber1 = db.Column(db.String(32), nullable=True)
+  phonenumber2 = db.Column(db.String(32), nullable=True)
   documentimages = db.relationship('DocumentImage', backref='patient', lazy='dynamic')
-   
-  def __init__(self, firstname, middlename, lastname, dob, phonenumber1, phonenumber2):
-    self.firstname = firstname
-    self.middlename = middlename
-    self.lastname = lastname
-    self.dob = dob
-    self.phonenumber1 = phonenumber1
-    self.phonenumber2 = phonenumber2
+  householdsize = db.Column(db.Integer, nullable=True)
+  householdincome = db.Column(db.Integer, nullable=True)
 
 class DocumentImage(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   patientid = db.Column(db.Integer, db.ForeignKey("patient.id"))
   filename = db.Column(db.String(64))
-
-  def __init__(self, patientId, filename):
-    self.patientid = patientId
-    self.filename = filename
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request,flash, redirect, url_for, g, send_from_directory
+from flask import render_template, request,flash, redirect, url_for, g, send_from_directory, session
 from app import app, db, bcrypt, ALLOWED_EXTENSIONS
 from app.models import Patient, DocumentImage, User
 import datetime
@@ -18,7 +18,8 @@ def login():
         db.session.commit()
         login_user(user, remember=True)
         return redirect(url_for('index'))
-  return render_template("login.html")
+  else:
+    return render_template("login.html")
 
 @app.route("/logout", methods=["GET"])
 @login_required
@@ -39,20 +40,21 @@ def before_request():
   g.user = current_user
 
 def allowed_file(filename):
-  return '.' in filename and \
-    filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+  return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route('/add' , methods=['POST', 'GET'])
+@app.route('/new_patient' , methods=['POST', 'GET'])
 @login_required
-def add():
+def new_patient():
   if request.method == 'POST':
     patient=Patient(
-      request.form['firstname'],
-      request.form['middlename'],
-      request.form['lastname'],
-      datetime.datetime.strptime(request.form['dob'], '%m/%d/%Y').date(),
-      request.form['phonenumber1'],
-      request.form['phonenumber2']
+      firstname = request.form['firstname'],
+      middlename = request.form['middlename'],
+      lastname = request.form['lastname'],
+      dob = datetime.datetime.strptime(request.form['dob'], '%m/%d/%Y').date(),
+      phonenumber1 = request.form['phonenumber1'],
+      phonenumber2 = request.form['phonenumber2'],
+      householdsize = request.form['householdsize'],
+      householdincome = request.form['householdincome']
     )
     db.session.add(patient)
     db.session.commit()
@@ -62,32 +64,49 @@ def add():
         filename = secure_filename(file.filename)
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(filepath)
-      documentImage = DocumentImage(patient.id, filename)
-      db.session.add(documentImage)
-
-    db.session.commit()
-
-    flash('New patient added!')     
+        documentImage = DocumentImage(patient.id, filename)
+        db.session.add(documentImage)
+        db.session.commit()
+   
     return redirect(url_for('index'))
+  else:
+    # Check whether we already have some data from a pre-screening
+    if 'household_size' in session or 'household_income' in session:
+      patient = Patient(
+        householdsize = session.get('household_size'),
+        householdincome = session.get('household_income')
+      )
+      session.clear()
+      return render_template('patient_details.html', patient=patient)
 
-  return render_template('add.html')
+    return render_template('patient_details.html')
 
-@app.route('/update/<id>', methods=['POST', 'GET'])
+@app.route('/patient_details/<id>', methods=['POST', 'GET'])
 @login_required
-def update(id):
+def patient_details(id):
   patient = Patient.query.get(id)
   if request.method == 'POST':
-    patient.firstname = request.form['firstname'],
-    patient.middlename = request.form['middlename'],
-    patient.lastname = request.form['lastname'],
-    patient.dob = datetime.datetime.strptime(request.form['dob'], '%m/%d/%Y').date(),
-    patient.phonenumber1 = request.form['phonenumber1'],
+    patient.firstname = request.form['firstname']
+    patient.middlename = request.form['middlename']
+    patient.lastname = request.form['lastname']
+    patient.dob = datetime.datetime.strptime(request.form['dob'], '%m/%d/%Y').date()
+    patient.phonenumber1 = request.form['phonenumber1']
     patient.phonenumber2 = request.form['phonenumber2']
-    db.session.commit()
-    flash('Patient updated!')
-    return redirect(url_for('index'))
+    patient.householdsize = request.form['householdsize']
+    patient.householdincome = request.form['householdincome']
 
-  return render_template('update.html', patient=patient)
+    for file in request.files.itervalues():
+      if allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+        documentImage = DocumentImage(patient.id, filename)
+        db.session.add(documentImage)
+
+    db.session.commit()
+    return redirect(url_for('index'))
+  else:
+    return render_template('patient_details.html', patient=patient)
 
 @app.route('/delete/<id>', methods=['POST', 'GET'])
 @login_required
@@ -95,7 +114,6 @@ def delete(id):
   patient = Patient.query.get(id)
   db.session.delete(patient)
   db.session.commit()
-  flash('Patient deleted!')
   return redirect(url_for('index'))
 
 @app.route('/document_image/<image_id>')
@@ -108,11 +126,65 @@ def document_image(image_id):
 @app.route('/documentimages/<filename>')
 @login_required
 def get_image(filename):
-  return send_from_directory(os.path.join(app.config['PROJECT_ROOT'], app.config['UPLOAD_FOLDER']), filename)
+  return send_from_directory(os.path.join(
+    app.config['PROJECT_ROOT'],
+    app.config['UPLOAD_FOLDER']),
+    filename
+  )
+
+@app.route('/new_prescreening/<patient_id>', methods=['POST', 'GET'])
+@app.route('/new_prescreening', defaults={'patient_id': None}, methods=['POST', 'GET'])
+@login_required
+def new_prescreening(patient_id):
+  if request.method == 'POST':
+    session['services'] = request.form.getlist('services')
+    return redirect(url_for('prescreening_basic'))
+  if patient_id is not None:
+    session.clear()
+    session['patient_id'] = patient_id
+  return render_template('new_prescreening.html')
+
+@app.route('/prescreening_basic', methods=['POST', 'GET'])
+@login_required
+def prescreening_basic():
+  if request.method == 'POST':
+    session['household_size'] = request.form['household_size']
+    session['household_income'] = request.form['household_income']
+    return redirect(url_for('prescreening_results'))
+  else:
+    if session.get('patient_id'):
+      patient = Patient.query.get(session['patient_id'])
+      return render_template('prescreening_basic.html', patient = patient)
+    else:
+      return render_template('prescreening_basic.html')
+
+@app.route('/prescreening_results')
+@login_required
+def prescreening_results():
+  if 'patient_id' in session and session['patient_id']:
+    return render_template(
+      'prescreening_results.html',
+      patientid = session['patient_id'],
+      services = session['services']
+    ) 
+  else:
+    return render_template('prescreening_results.html', services = session['services'])
+
+@app.route('/save_prescreening_updates')
+@login_required
+def save_prescreening_updates():
+  if 'patient_id' in session and session['patient_id']:
+    patient_id = session['patient_id']
+    patient = Patient.query.get(session['patient_id'])
+    patient.householdsize = session['household_size']
+    patient.householdincome = session['household_income']
+    db.session.commit()
+    session.clear()
+    return redirect(url_for('patient_details', id = patient_id))
 
 @app.route('/' )
 @login_required
 def index():
+  session.clear()
   patients = Patient.query.all()    
-  print patients[2].documentimages
   return render_template('index.html', patients=patients)
