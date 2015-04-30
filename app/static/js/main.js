fd2a64ca5a3b2cfb571f6c3de6cbecd3eb7adf67
svmatthews <matthews.sam@gmail.com>
2015-04-30 14:16:03 -0700

    last js test

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,9 +1,9 @@
 window.App = window.App || {};
 
 var AppController = function ( options ) {
+  console.info('APP INITIALIZED :)');
   this.options = options || {};
   this.waka = 'flaka';
   this.hiphip = 'hooray!';
-  console.info('APP INITIALIZED :)');
   console.log(this);
 };
