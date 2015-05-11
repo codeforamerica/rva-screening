81598e6563f8330d270adf9fee20dc54eda81aeb
svmatthews <matthews.sam@gmail.com>
2015-05-11 10:57:58 -0700

    adding search functionality to landing page

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -3,7 +3,17 @@ window.App = window.App || {};
 var AppController = function ( options ) {
   console.info('APP INITIALIZED :)');
   this.options = options || {};
-  this.waka = 'flaka';
   this.hiphip = 'hooray!';
   console.log(this);
+
+  if ($('#patient-search').length) {
+    this.search = {};
+    this.initSearch('patient-search', { valueNames: ['patient-name', 'patient-dob'] });
+  }
 };
+
+AppController.prototype.initSearch = function ( id, options ) {
+  this.search.id = id;
+  this.search.options = options;
+  this.search.list = new List(id, options);
+}
