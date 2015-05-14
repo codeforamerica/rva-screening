7854bcd663c22208421eefc4fb9486dcce7c0fbc
Benjamin Golder <benjamin.j.golder@gmail.com>
2015-05-14 00:48:06 -0700

    adding flask-s3 to upload static assets to amazon s3

--- config.py
+++ config.py
@@ -3,3 +3,7 @@ import os
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql://root:password@localhost/rva_screening')
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 IS_PRODUCTION = os.environ.get('IS_PRODUCTION', False)
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+S3_BUCKET_NAME = 'rva-screener'
+S3_ONLY_MODIFIED = False
