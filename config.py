fede00f401647e5f1b12c3bf13c469a89d16085b
Benjamin Golder <benjamin.j.golder@gmail.com>
2015-05-14 10:32:06 -0700

    removing IS_PRODUCTION environmental variable, replaced by SCREENER_ENVIRONMENT (=prod if production)

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql://root:password@localhost/rva_screening')
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-IS_PRODUCTION = os.environ.get('IS_PRODUCTION', False)
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 SCREENER_ENVIRONMENT = os.environ.get('SCREENER_ENVIRONMENT')
