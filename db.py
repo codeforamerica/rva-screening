23742bb53cd4e9291c21c0689290424a4d4bc241
Emma Smithayer <esmithayer@gmail.com>
2015-07-07 18:00:45 -0700

    make db.py work with blueprints

--- db.py
+++ db.py
@@ -1,7 +1,8 @@
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
-from app import app, db
+from app import create_app, db
 
+app = create_app()
 migrate = Migrate(app, db)
 
 manager = Manager(app)
