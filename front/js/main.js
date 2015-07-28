901ce0a824bbbce33b1462c2957ac85ea5b1f3e1
mapsam <matthews.sam@gmail.com>
2015-07-28 10:26:43 -0700

    move filter related scripts into filter.js, #183

--- front/js/main.js
+++ front/js/main.js
@@ -61,47 +61,6 @@ var AppController = function ( options ) {
     convertForPrint();
   }
 
-  /*
-  **  PATIENT DETAILS / UNIQUE QUESTIONS FILTERS
-  **  Shows/hides service-specific questions on the page based on
-  **  checkbox values
-  **
-  **  TODO: move into filters.js
-  **
-  */
-  var serviceFilters = $('.patient_details_filter .field_checkbox input[type="checkbox"]');
-  // var allUniqueQuestions = $('.q_unique');
-  serviceFilters.on('change', function(e) {
-    // var checked = [];
-
-    // // get all checked filters
-    // serviceFilters.each(function(){
-    //   if($(this).is(':checked')) {
-    //     checked.push($(this).attr('name'));
-    //   }
-    // });
-
-    // // hide all question fields
-    // allUniqueQuestions.hide();
-
-    // // now bring them back if they match the checked
-    // // boxes on the filters
-    // allUniqueQuestions.each(function(){
-    //   var q = $(this);
-    //   for (var c = 0; c < checked.length; c++) {
-    //     if (q.hasClass(checked[c])) {
-    //       q.show();
-    //     }
-    //   }
-    // });
-    var select = $('.'+$(this).attr('name'));
-    if ($(this).is(':checked')) {
-      select.show();
-    } else {
-      select.hide();
-    }
-  });
-
 };
 
 
